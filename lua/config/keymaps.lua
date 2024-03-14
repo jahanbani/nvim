ed67f00ee92e0a6ea20fb27b92f4d526918ea5bd
Jahanbani Ardakani <alij@iastate.edu>
2024-03-14 17:27:07 -0500

    mychanges

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Save key strokes (now we do not need to press shift to enter command mode).
+vim.keymap.set({ "n", "x" }, ";", ":")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set the font in Neovim's GUI
+vim.opt.guifont = "FiraCode Nerd Font Mono:h13"
+-- Enable text wrapping
+vim.opt.wrap = true
+-- Break line at predefined characters
+vim.opt.linebreak = true
+-- Character to show before the lines that have been soft-wrapped
+vim.opt.showbreak = "↪"
+-- Disable swap file
+vim.opt.swapfile = false
